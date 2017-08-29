8df3d47cfd3f59b18f59cfff14ca9130dfb7896c
CristianHoyos1313 <30868645+CristianHoyos1313@users.noreply.github.com>
2017-08-29 08:29:18 -0500

    Update persona.java

--- PROYECTOS/src/src/estructurasdedatos/cap1/tiposdedatos/persona.java
+++ PROYECTOS/src/src/estructurasdedatos/cap1/tiposdedatos/persona.java
@@ -1,21 +1,40 @@
 package estrucutrasdedatos.cap1.tipsdedatos
 
-public class Persona
-{ 
-  private String cedula;
-  private String nombre;
-  private String telefono;
-  private String direccion;
+public class Persona {  
+    private String nombre;
+    private String cedula;
+    private String direccion;
+    private String telofono;
+   
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+   public String getCedula() {
+        return cedula;
+    }
   
-  
-  public String getCedula(){
-  	String salida;
-    salida=this.cedula;
-    return salida;
-  }
-  
-  public void setCedula(String entrada){
-  	this.cedula;
-    return salida;
-  }
-  }
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+ 
+    public String getDireccion() {
+        return direccion;
+    }
+    
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelofono() {
+        return telofono;
+    }
+   
+    public void setTelofono(String telofono) {
+        this.telofono = telofono;
+    } 
+}
