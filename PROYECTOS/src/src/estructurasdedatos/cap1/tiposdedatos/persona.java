b2abee46210c10633b229af827279d60593b81b4
CristianHoyos1313 <30868645+CristianHoyos1313@users.noreply.github.com>
2017-08-28 21:28:18 -0500

    Create persona.java

---
+++ PROYECTOS/src/src/estructurasdedatos/cap1/tiposdedatos/persona.java
@@ -0,0 +1,21 @@
+package estrucutrasdedatos.cap1.tipsdedatos
+
+public class Persona
+{ 
+  private String cedula;
+  private String nombre;
+  private String telefono;
+  private String direccion;
+  
+  
+  public String getCedula(){
+  	String salida;
+    salida=this.cedula;
+    return salida;
+  }
+  
+  public void setCedula(String entrada){
+  	this.cedula;
+    return salida;
+  }
+  }
