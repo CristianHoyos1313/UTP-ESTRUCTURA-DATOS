5c851da7055f581aadd232c5ee0bc4a91776ed15
CristianHoyos1313 <30868645+CristianHoyos1313@users.noreply.github.com>
2017-08-28 21:53:03 -0500

    Create FigureGeometric.java

---
+++ PROYECTOS/src/src/estructurasdedatos/cap1/tiposdedatos/FigureGeometric.java
@@ -0,0 +1,21 @@
+public class GeometricFigure
+{ 
+  private String codigo;
+  public  String Tipo;
+  private String nombre;
+
+  public int getCodigo() {
+        return codigo;
+    }
+  
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+  public int getTipo() {
+        return tipo;
+    }
+  
+    public void setCodigo(String codigo) {
+        this.tipo = codigo;
+    }
+}
