0861d94262abd0431dea72da73c9906beb42adfd
CristianHoyos1313 <30868645+CristianHoyos1313@users.noreply.github.com>
2017-10-30 21:25:51 -0500

    Update ArbolB.java

--- PROYECTOS/src/src/estructurasdedatos/cap4/arboles/ArbolB.java
+++ PROYECTOS/src/src/estructurasdedatos/cap4/arboles/ArbolB.java
@@ -1,14 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package estructurasdedatos.cap4.arboles;
 
-/**
- *
- * @author tusk
- */
-public class ArbolB{
 
+
+public class ArbolB {
+// llamamos el nodo
+    private NodoArbolB raiz;
+    // funcion booleada  para saber si esta vacio
+    public boolean esVacia() {
+        return raiz == null;
+    }
+    public void insertar (int nodo1) {
+        if (esVacia()) {
+            NodoArbolB nuevonodo = new NodoArbolB();
+            nuevonodo.dato = nodo1;
+            nuevonodo.hijoderecho = new ArbolB();
+            nuevonodo.hijoizquierdo = new ArbolB();
+            raiz = nuevonodo;
+        } else {
+            if (nodo1 > raiz.dato) {
+                raiz.hijoderecho.insertar(nodo1);
+            }
+            if (nodo1 < raiz.dato) {
+                raiz.hijoizquierdo.insertar(nodo1);
+            }
+        }
+    }
+    public void orden() {
+        if (!esVacia()) {
+            raiz.hijoizquierdo.orden();
+            System.out.println(raiz.dato + ", ");
+            raiz.hijoderecho.orden();
+        }
+    }
 }
